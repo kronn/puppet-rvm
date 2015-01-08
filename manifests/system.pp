5539f97876dce5aa9011e16c4a483b621b7fe6d6
Matthias Viehweger <kronn@kronn.de>
2015-01-08 14:33:54 +0100

    Retrieve and import the gpg-key used for signing RVM

--- manifests/system.pp
+++ manifests/system.pp
@@ -6,12 +6,21 @@ class rvm::system($version=undef) {
     default   => $version,
   }
 
+  exec { 'system-rvm-gpg-key':
+    command     => 'gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3',
+    path        => $::path,
+    environment => $environment,
+    unless      => 'gpg2 --list-keys 409B6B1796C275462A1703113804BB82D39DC0E3',
+    require     => Class['::rvm::gpg']
+  }
+
   exec { 'system-rvm':
     path        => '/usr/bin:/usr/sbin:/bin',
     command     => "/usr/bin/curl -fsSL https://get.rvm.io | bash -s -- --version ${actual_version}",
     creates     => '/usr/local/rvm/bin/rvm',
     require     => [
       Class['rvm::dependencies'],
+      Exec['system-rvm-gpg-key'],
     ],
   }
 
