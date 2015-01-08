bf5f5903b29e3d89ab5e8f5c4b6a8d3916d39f67
Matthias Viehweger <kronn@kronn.de>
2015-01-08 14:28:03 +0100

    Switch to new source of rvm-script

--- manifests/system.pp
+++ manifests/system.pp
@@ -7,13 +7,10 @@ class rvm::system($version=undef) {
   }
 
   exec { 'system-rvm':
-    path    => '/usr/bin:/usr/sbin:/bin',
-    command => "bash -c '/usr/bin/curl -sL https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
-                chmod +x /tmp/rvm-installer && \
-                rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
-                rm /tmp/rvm-installer'",
-    creates => '/usr/local/rvm/bin/rvm',
-    require => [
+    path        => '/usr/bin:/usr/sbin:/bin',
+    command     => "/usr/bin/curl -fsSL https://get.rvm.io | bash -s -- --version ${actual_version}",
+    creates     => '/usr/local/rvm/bin/rvm',
+    require     => [
       Class['rvm::dependencies'],
     ],
   }
